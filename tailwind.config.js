e32cad3469feac42a477c449074f23923b72519e
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-09-26 07:00:30 +0100

    Set up the variables and delete unwanted stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,73 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	darkMode: ["class"],
+	content: [
+		"./pages/**/*.{js,jsx}",
+		"./components/**/*.{js,jsx}",
+		"./app/**/*.{js,jsx}",
+		"./src/**/*.{js,jsx}",
+	],
+	theme: {
+		screens: {
+			xs: "380px",
+			sm: "480px",
+			md: "600px",
+			mn: "725px",
+			dm: "901px",
+			lg: "976px",
+			xl: "1440px",
+		},
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
+		extend: {
+			colors: {
+				background: "var(--background)",
+				text: "var(--text)",
+
+				secondary: "var(--secondary)",
+				tertiary: "var(--tertiary)",
+				hovery: "var(--hovery)",
+
+				brand: "var(--brand)",
+				orange: "var(--orange)",
+				red: "var(--red)",
+				purple: "var(--purple)",
+				lightpurple: "var(--lightpurple)",
+				green: "var(--green)",
+			},
+			fontSize: {
+				xlarge: "var(--xlarge)",
+				large: "var(--large)",
+				medium: "var(--medium)",
+				normal: "var(--normal)",
+				small: "var(--small)",
+				tiny: "var(--tiny)",
+			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
+			},
+			keyframes: {
+				"accordion-down": {
+					from: { height: 0 },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: 0 },
+				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+			},
+		},
+	},
+	plugins: [require("tailwindcss-animate")],
+};
