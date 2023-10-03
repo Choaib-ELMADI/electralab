b46a93aae68c6a249580b01b9c17dedf1c2580c5
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-10-03 18:00:06 +0100

    Fix the support component

--- components/utils/support.jsx
+++ components/utils/support.jsx
@@ -21,19 +21,21 @@ const support = [
 
 const Support = () => {
 	return (
-		<div className="w-full max-w-[600px] mx-auto px-2 py-4 flex flex-col gap-4 items-center text-background dark:text-text bg-gradient-to-r from-pink to-purple rounded-lg relative after:absolute after:inset-[-2px] after:bg-gradient-to-r after:from-purple after:to-pink after:z-[-1] after:rounded-lg">
-			<h1 className="text-medium text-center">Thanks For Supporting Us</h1>
-			<div className="flex gap-4 items-center">
-				{support.map((s) => (
-					<Link
-						key={s.name}
-						href={s.link}
-						target="_blank"
-						className="hover:text-[#000] transition-all"
-					>
-						<s.icon size={24} />
-					</Link>
-				))}
+		<div className="w-full max-w-[600px] mx-auto bg-gradient-to-r from-purple to-pink rounded-lg p-[2px] flex items-center justify-center">
+			<div className="w-full px-2 py-4 flex flex-col gap-4 items-center text-background dark:text-text bg-gradient-to-r from-pink to-purple rounded-lg">
+				<h1 className="text-medium text-center">Thanks For Supporting Us</h1>
+				<div className="flex gap-4 items-center">
+					{support.map((s) => (
+						<Link
+							key={s.name}
+							href={s.link}
+							target="_blank"
+							className="hover:text-[#000] transition-all"
+						>
+							<s.icon size={24} />
+						</Link>
+					))}
+				</div>
 			</div>
 		</div>
 	);
